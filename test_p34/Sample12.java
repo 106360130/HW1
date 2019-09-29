a32955e8777bc3c0e08df82aacd604f60babe8ba
106360130 <bs25017@gmail.com>
2019-09-29 21:22:17 +0800

    first

---
+++ test_p34/Sample12.java
@@ -0,0 +1,27 @@
+package test_p34;
+
+public class Sample12
+{
+	public static void main(String[] arg)
+	{
+		int num1 = 10;
+		int num2 = 5;
+		
+		System.out.println("nmu1㎝nmu2贺笲衡:");
+		System.out.println("nmu1+num2单" + (num1+num2));
+		System.out.println("nmu1-num2单" + (num1-num2));
+		System.out.println("nmu1*num2单" + (num1*num2));
+		System.out.println("nmu1/num2单" + (num1/num2));
+		System.out.println("nmu1%num2单" + (num1%num2));
+		
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		
+		b = a++;
+		c = ++a;
+		
+		System.out.println("琌﹚ぇ患糤┮b" + b);
+		System.out.println("琌患糤ぇ﹚┮c" + c);
+	}
+}
